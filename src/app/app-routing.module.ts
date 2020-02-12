817dc344500e19e6ba2883d3a5e22ab407adba60
Faisal Khan <faisal.khan@appinventiv.com>
2020-02-12 13:38:17 +0530

    n

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ACCOUNT } from './constants/routes';
 
 const routes: Routes = [
   { path: ACCOUNT, loadChildren: () => import('./modules/accounts/accounts.module').then(m => m.AccountsModule) },
+  { path: '', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];
 
 @NgModule({
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const inrRoute: Routes = [
+  { path: '', component: DashboardComponent }
+]
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(inrRoute),
   ]
 })
 export class DashboardModule { }
+
+
